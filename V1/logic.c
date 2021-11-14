a8c157512d7414527d8e8718d27198942b977476
aitouali1u <aitouali1u@etu.univ-lorraine.fr>
2021-11-14 17:49:08 +0100

    Depot du code des headers

---
+++ V1/logic.c
@@ -0,0 +1,77 @@
+/**
+ * \file logic.c
+ * \brief Gestion de la logique du jeu (physique)
+ * \author DJERMOUNI Yanis - YATIME Marouane
+ * \date 26 mai 2021
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sdl2.h"
+#include "const.h"
+
+#include "logic.h"
+#include "data.h"
+
+
+
+/**
+ * \brief gere les collisions entre le sprite et l'écran (gauche et droite)
+ * \param s le sprite
+ */
+void screen_collide(sprite_t* sprite)
+{
+    //left
+    if(sprite->x < 145)  // Trouvé a l aide des pixels du background
+        sprite->x = 145;
+    //right
+    if(sprite->x + sprite->w > 700)  // 700 taille de la piste de route 
+        sprite->x = 700 - sprite->w;
+}
+
+
+/**
+ * \brief gere les collisions entre deux sprites
+ * \param sp1 un sprite
+ * \param sp2 un sprite
+ * \return 1 si collision, 0 sinon
+ */
+int sprites_collide(sprite_t *sp1, sprite_t *sp2){
+    if(abs(sp1->x + sp1->w/2 - sp2->x - sp2->w/2) <= (sp1->w + sp2->w)/2 && abs(sp1->y + sp1->h/2 - sp2->y - sp2->h/2) <= (sp1->h + sp2->h)/2)
+        return 1;
+    return 0;
+}
+
+
+/**
+ * \brief rend la vitesse(world.vy) nulle lors de la collision
+ * \param sp1 un sprite
+ * \param sp2 un sprite
+ * \param world les données du monde
+ * \param make_disappear vaut 1 si on doit faire disparaître le sprite, 0 si on ne veut pas, autre cas particulier
+ * \return 1 si une meteore doit être détruite, 0 sinon
+ */
+void handle_sprites_collision(sprite_t *sp1, sprite_t *sp2, world_t *world)
+{
+    if(sprites_collide(sp1, sp2) == 1)
+    {
+        if((sp1->type == SPACESHIP && sp2->type == FINISH_LINE) || (sp2->type == SPACESHIP && sp1->type == FINISH_LINE))
+        {
+            world->spaceship.spaceship.is_visible = 0;
+            world->gameover = WIN;
+        }
+        else if(((sp1->type == SPACESHIP && sp2->type != FINISH_LINE) || (sp2->type == SPACESHIP && sp1->type != FINISH_LINE)) && (sp1->is_visible && sp2->is_visible))
+        {
+            world->spaceship.spaceship.is_visible = 0;
+            world->gameover = LOOSE;
+        }
+        else if((sp1->type == BULLET && sp2->type == METEORE && sp2->is_visible) || (sp2->type == BULLET && sp1->type == METEORE && sp1->is_visible))
+        {
+            sp2->is_visible = 0;
+            sp1->is_visible = 0;
+        }
+
+    }
+    if(world->gameover == CONTINUE)
+        world->gameover = CONTINUE;
+}
---
+++ V1/main.c
@@ -0,0 +1,138 @@
+/**
+ * \file main.c
+ * \brief Boucle de jeu
+ * \author DJERMOUNI Yanis - YATIME Marouane
+ * \date 26 mai 2021
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sdl2.h"
+#include "sdl2-ttf.h"
+#include "const.h"
+#include "graphics.h"
+#include "logic.h"
+#include "data.h"
+#include "event.h"
+
+
+
+/**
+ * \brief fonction qui initialise le jeu: initialisation de la partie graphique (SDL), chargement des textures, initialisation des données
+ * \param window la fenêtre du jeu
+ * \param renderer le renderer
+ * \param ressources les ressources
+ * \param world le monde
+ */
+void init(SDL_Window **window, SDL_Renderer ** renderer, ressources_t *ressources, world_t * world){
+    init_sdl(window,renderer,SCREEN_WIDTH, SCREEN_HEIGHT);
+    init_ttf();
+    init_data(world);
+    init_textures(*renderer,ressources);
+}
+
+
+/**
+* \brief fonction qui nettoie le jeu: nettoyage de la partie graphique (SDL), nettoyage des textures, nettoyage des données
+* \param window la fenêtre du jeu
+* \param renderer le renderer
+* \param ressources les ressources
+* \param world le monde
+*/
+void clean(SDL_Window *window, SDL_Renderer * renderer, ressources_t *ressources, world_t * world){
+    clean_data(world);
+    clean_textures(ressources);
+    clean_ttf();
+    clean_sdl(renderer,window);
+}
+
+
+/**
+* \brief fonction qui permet de relancer la boucle de jeu après une partie finie
+* \param world le monde
+*/
+void re_init(world_t * world)
+{
+    clean_data(world);
+    init_data(world);
+    world->t0 = SDL_GetTicks();
+}
+
+
+
+/**
+ *  \brief programme principal qui implémente la boucle du jeu
+ */
+int main(int argc, char* args[])
+{
+    SDL_Event event;
+    world_t world;
+    ressources_t ressources;
+    SDL_Renderer *renderer;
+    SDL_Window *window;
+
+    //initialisation du jeu
+    init(&window,&renderer,&ressources,&world);
+
+    //affichage du menu dans le début 
+    print_menu_debut(renderer, &ressources, &world);
+    while(1) {
+        if(SDL_PollEvent(&event)) {
+            //ENTRER est pressée
+            if(event.key.keysym.sym == SDLK_RETURN
+            || event.key.keysym.sym == SDLK_RETURN2
+            || event.key.keysym.sym == SDLK_KP_ENTER) { break; }
+
+            //ECHAP ou si on quitte
+            if(event.type == SDL_QUIT
+            || event.key.keysym.sym == SDLK_ESCAPE)
+            {
+                clean(window,renderer,&ressources,&world);
+                return 0;
+            }
+        }
+    }
+
+    world.t0 = SDL_GetTicks();
+
+    while(1){ //tant que le jeu n'est pas fini
+        //gestion des évènements
+        handle_events(&event,&world);
+        
+        //mise à jour des données liée à la physique du monde
+        update_data(&world);
+        
+        //rafraichissement de l'écran
+        refresh_graphics(renderer, &world,&ressources);
+
+        //pause de 10 ms pour controler la vitesse de rafraichissement
+        pause(10);
+
+        if(is_game_over(&world))
+        {
+            pause(2000);
+
+            //afichage du menu dans la fin du jeu 
+            print_menu_fin(renderer, &ressources, &world);
+            while(1) {
+                if(SDL_PollEvent(&event)) {
+                    //ENTRER est pressée
+                    if(event.key.keysym.sym == SDLK_RETURN
+                    || event.key.keysym.sym == SDLK_RETURN2
+                    || event.key.keysym.sym == SDLK_KP_ENTER) { re_init(&world); break;}
+                    
+                    //ECHAP ou si on quitte
+                    if(event.type == SDL_QUIT
+                    || event.key.keysym.sym == SDLK_ESCAPE)
+                    {
+                        clean(window,renderer,&ressources,&world);
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+
+    //Jamais atteint
+    return 0;
+}
